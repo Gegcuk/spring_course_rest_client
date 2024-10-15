707c3fe1e9b4d0f704089e5141841de7395698e9
alekseiLazunin <gegcuk@gmail.com>
2024-10-15 02:11:06 +0100

    init: Add lightweight REST client for study project
    - Created `App` class to interact with REST API, retrieving and manipulating employee data.
    - Implemented `Communication` class to handle REST calls (GET, POST, PUT, DELETE) using `RestTemplate`.
    - Added `Employee` entity to represent employee data.
    - Configured `MyConfig` class to provide `RestTemplate` bean for REST client operations.
    - Integrated functionality to fetch all employees, get employee by ID, save (add/update), and delete employees.

--- src/main/java/uk/gegc/App.java
+++ src/main/java/uk/gegc/App.java
@@ -1,5 +1,11 @@
 package uk.gegc;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import uk.gegc.configuration.MyConfig;
+import uk.gegc.entity.Employee;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +15,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+
+        Employee empById = communication.getEmployee(2);
+        System.out.println(empById);
+
+//        Employee employee = new Employee("Svetlana", "Sokolova", "IT", 1500);
+//        communication.saveEmployee(employee);
+
+//        communication.deleteEmployee(17);
     }
 }
